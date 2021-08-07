caaba64675d3997484ab36ed68efb6a988803988
jsorbello27 <j.sorbello27@gmail.com>
2021-08-06 22:16:53 -0500

    finished exercise

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,35 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String args[]) {
-        String question = "";
-        String choiceOne = "";
-        String choiceTwo = "";
-        String choiceThree = "";
+        String question = "Is Jude cutest baby alive?";
+        String choiceOne = "yes";
+        String choiceTwo = "no";
+        String choiceThree = "maybe";
 
-        String correctAnswer = choiceTwo;
+
+        String correctAnswer = choiceOne;
 
         // Write a print statement asking the question
+        System.out.println(question);
         // Write a print statement giving the answer choices
+        System.out.println(choiceOne +"\n" +choiceTwo +"\n"  + choiceThree);
 
         // Have the user input an answer
+        Scanner scanner = new Scanner(System.in);
         // Retrieve the user's input
+        String input = scanner.next();
+
+
 
         // If the user's input matches the correctAnswer...
         // then the user is correct and we want to print out a congrats message to the user.
-
+        if(correctAnswer.equals(input.toLowerCase())){
+            System.out.println("Correct!");
+        }else{
+            System.out.println("Wrong Loser!");
+        }
         // If the user's input does not match the correctAnswer...
         // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
 
